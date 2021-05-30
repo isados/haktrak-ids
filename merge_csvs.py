d175b746f8726d1d84702aa90f9d8dfa2b84b939
Isa AlDoseri <isadosry@gmail.com>
2021-05-30 13:26:36 +0300

    can now delete old csv files

--- merge_csvs.py
+++ merge_csvs.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import glob
 import pandas as pd
 from utilities import get_configs
@@ -9,8 +10,23 @@ prefix_text = "2021" # Starts with current year
 
 select_files = glob.glob(f"{root}/{prefix_text}*")
 df_from_each_file = (pd.read_csv(f, sep=',') for f in select_files)
-df_merged = pd.concat(df_from_each_file, ignore_index=True)
+    
+try:
+    df_merged = pd.concat(df_from_each_file, ignore_index=True)
+except ValueError as e:
+    print("Error: %s" % e)
+    sys.exit(1)
+
 
 save_path = configvars['CompanyDataset']['fullsize']
 df_merged.to_csv(save_path, index=False)
+
+# Delete the remaining csv files
+for file in select_files:
+    try:
+        os.remove(file)
+    except OSError as e:  ## if failed, report it back to the user ##
+        print ("Error: %s - %s." % (e.filename, e.strerror))
+        
+
 print("Done!")
