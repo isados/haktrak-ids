cd44ded56f1a43951f6d5f750953bae658db199f
Isa AlDoseri <isadosry@gmail.com>
2021-05-30 13:08:59 +0300

    first run of the config.yml

--- merge_csvs.py
+++ merge_csvs.py
@@ -1,13 +1,16 @@
 import os
 import glob
 import pandas as pd
+from utilities import get_configs
 
-root = "CompanyDataset"
+configvars = get_configs()
+root = configvars['CompanyDataset']['rootpath']
 prefix_text = "2021" # Starts with current year
 
 select_files = glob.glob(f"{root}/{prefix_text}*")
 df_from_each_file = (pd.read_csv(f, sep=',') for f in select_files)
 df_merged = pd.concat(df_from_each_file, ignore_index=True)
-save_path = os.path.join(root, "merged.csv")
+
+save_path = configvars['CompanyDataset']['fullsize']
 df_merged.to_csv(save_path, index=False)
 print("Done!")
--- utilities.py
+++ utilities.py
@@ -1,18 +1,24 @@
+import yaml
 import pandas as pd
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
-def read_base_dataset(dataset_path: str="CIC_IDS2017_Dataset/MachineLearningCVE/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv") -> pd.DataFrame:
-    
+def read_base_dataset(dataset_path: str, version: int=2018) -> pd.DataFrame:
     base_df = pd.read_csv(dataset_path)
 
-    # Clear extra whitespace within the columns
-    base_df.columns = base_df.columns.map(lambda x : x.strip())
-
-    # Remove unnecessary column
-    base_df.drop('Fwd Header Length.1', axis=1, inplace=True)
+    if version == 2018:
+        pass
+    else:
+        if dataset_path is None:
+            dataset_path = "CIC_IDS2017_Dataset/MachineLearningCVE/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv"
+        # Clear extra whitespace within the columns
+        base_df.columns = base_df.columns.map(lambda x : x.strip())
+        # Remove unnecessary column
+        base_df.drop('Fwd Header Length.1', axis=1, inplace=True)
 
     return base_df
 
-def read_companys_dataset(dataset_path: str="CompanyDataset/example.pcap_Flow.csv") -> pd.DataFrame:
+def read_companys_dataset2017(dataset_path: str="CompanyDataset/example.pcap_Flow.csv") -> pd.DataFrame:
 
     # Paths
     basedataset_path = "CIC_IDS2017_Dataset/MachineLearningCVE/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv"
@@ -70,3 +76,71 @@ def read_companys_dataset(dataset_path: str="CompanyDataset/example.pcap_Flow.cs
 
     return company_df
 
+def read_companys_dataset2018(company_dataset_path: str="CompanyDataset/example.pcap_Flow.csv") -> pd.DataFrame:
+    basedataset_path = "/home/haktrak/Public/DDosHOIC.csv"
+
+    base_df = pd.read_csv(basedataset_path)
+    company_df = pd.read_csv(company_dataset_path)
+
+    # Make Comparison
+
+    # Before processing columns
+    print("Number of Base Dataset Columns:", len(base_df.columns))
+    print("Number of Own Dataset Columns:", len(company_df.columns))
+
+    baseset = set(base_df.columns)
+    companyset = set(company_df.columns)
+
+
+    # Create a replacement mapping dictionary
+    different_colnames = companyset - baseset
+
+    # Pick scorer for the fuzzy matching
+    scorer = fuzz.token_sort_ratio
+    score_limit = 65 # Fixed value
+
+    corrected_labels_map = {}
+
+    for column_name in different_colnames:
+        correct_name = process.extractOne(column_name, baseset, scorer=scorer)
+        if correct_name[1] < score_limit:
+            continue # so basically skip
+        corrected_labels_map[column_name] = correct_name # add to mapping
+        
+
+    print(f"\nNumber of Columns to correct : {len(different_colnames)}")
+    print(f"Number of corrected labels from `{scorer.__name__}` : {len(corrected_labels_map)}")
+
+    names = corrected_labels_map.items()
+    corrected_labels_map = dict({entry[0]: entry[1][0] for entry in corrected_labels_map.items()})
+
+    # Rejected certain corrections from it
+    keys = ['Flow ID', 'Dst IP', 'Bwd Packet/Bulk Avg', 'Fwd Packet/Bulk Avg', 'ECE Flag Count']
+    for key in keys:
+        del corrected_labels_map[key]
+
+    # ... and add manual changes
+    change = {'Bwd Packet/Bulk Avg' : 'Bwd Pkts/b Avg',
+    'ECE Flag Count': 'ECE Flag Cnt',
+    'Fwd Packet/Bulk Avg': 'Fwd Pkts/b Avg',
+    'Total Length of Bwd Packet': 'TotLen Bwd Pkts',
+    'Total Length of Fwd Packet': 'TotLen Fwd Pkts'}
+
+    corrected_labels_map.update(change)
+
+    # Drop columns
+    extra_cols = companyset - corrected_labels_map.keys() - baseset
+    company_df.drop(extra_cols, axis=1, inplace=True)
+
+    # Finally replace columns
+    company_df.columns = company_df.columns.map(lambda bad_col: corrected_labels_map.get(bad_col, bad_col))
+
+    return company_df
+
+def get_configs(config_file: str="config.yml") -> dict:
+    try:
+        with open(config_file, 'r', newline='') as f:
+            return yaml.load(f, Loader=yaml.Loader)
+    except yaml.YAMLError as ymlexcp:
+        print(ymlexcp)
+        return None
