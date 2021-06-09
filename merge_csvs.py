bbcf0a5d47496d7b6d27132d40da9ec1a0b329cf
Isa AlDoseri <isadosry@gmail.com>
2021-06-09 10:36:01 +0300

    fixed the merged_csv file and is a more robust script now

--- merge_csvs.py
+++ merge_csvs.py
@@ -1,32 +1,54 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 import glob
+import argparse
+import numpy as np
 import pandas as pd
 from utils import get_configs
 
-configvars = get_configs()
-root = configvars['CompanyDataset']['rootpath']
-prefix_text = "2021" # Starts with current year
+parser = argparse.ArgumentParser()
+parser.add_argument("csv", 
+                    help="path to csv directory", metavar="CSV_FOLDER")
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="have output verbosity")
+parser.add_argument("-p", "--prefix", default="",
+                    help="filter by a prefix")
+parser.add_argument("-c", "--columns",
+                    help="mention any columns to use instead of the header in the csv")
+parser.add_argument("-o", "--savepath", 
+                    help="save path of merged csv")
+args = parser.parse_args()
 
-select_files = glob.glob(f"{root}/{prefix_text}*")
-df_from_each_file = (pd.read_csv(f, sep=',') for f in select_files)
-    
+configvars = get_configs()
+
+location = args.csv
+csv_files = glob.glob(f"{location}/{args.prefix}*")
+df_from_each_file = [pd.read_csv(f, sep=',', header=None) for f in csv_files]
+raw_values = [df.values for df in df_from_each_file]
+
 try:
-    df_merged = pd.concat(df_from_each_file, ignore_index=True)
+    raw_array = np.concatenate(raw_values, axis=0)
+    df_merged = pd.DataFrame(raw_array, columns=[args.columns])
 except ValueError as e:
     print("Error: %s" % e)
     sys.exit(1)
 
 
-save_path = configvars['CompanyDataset']['fullsize']
-df_merged.to_csv(save_path, index=False)
+savepath = args.savepath
+if savepath is None:
+    new_csvname = f"{os.path.basename(location)}_merged.csv"
+    savepath = os.path.join(location, new_csvname)
+
+df_merged.to_csv(savepath, index=False)
 
 # Delete the remaining csv files
-for file in select_files:
-    try:
-        os.remove(file)
-    except OSError as e:  ## if failed, report it back to the user ##
-        print ("Error: %s - %s." % (e.filename, e.strerror))
+# for file in csv_files:
+#     try:
+#         os.remove(file)
+#     except OSError as e:  ## if failed, report it back to the user ##
+#         print ("Error: %s - %s." % (e.filename, e.strerror))
         
 
 print("Done!")
